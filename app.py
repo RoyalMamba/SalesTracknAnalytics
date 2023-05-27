c2b2bdd0ad194ac866fd0ecd1ae5f2cfd8b380d2
Saurabh Rajaram Yadav <ysaurabh059@gmail.com>
2023-05-27 19:44:27 +0530

    Debugging dynamic columns

--- app.py
+++ app.py
@@ -161,6 +161,8 @@ class SaveData:
             self.availability.to_excel(writer, sheet_name='remaining_cards' , index = False)
 
 def getStockreport():
+    # month = int(input('Enter the month: '))
+    # year = int(input("Enter the year: "))
     month = datetime.now().month
     year = datetime.now().year
 
@@ -216,6 +218,7 @@ def get_sales_data():
     sales_data = getStockreport()
     sales_data_json = sales_data.dailysales.astype('int').reset_index().to_json(orient='records')
     columns = sales_data.dailysales.reset_index().columns
+    # print(columns)
     sales_data_list = json.loads(sales_data_json)
     # print(sales_data.dailysales.astype('int').reset_index())
 
