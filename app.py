4d4d6a6a75966c67975c772a57d9f354026b0720
Saurabh Rajaram Yadav <ysaurabh059@gmail.com>
2023-07-13 22:47:35 +0530

    Render Version

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ class CardStatus:
 
     def fetch_status(self, month, year):
         status = []
-        with concurrent.futures.ThreadPoolExecutor(max_workers=216) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=700) as executor:
             futures = [executor.submit(self.make_request, i, month, year) for i in self.yetTOcome['SRC No']]
             for future in concurrent.futures.as_completed(futures):
                 result = future.result()
